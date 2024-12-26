b545b311d4cae97eb203746062696458f2c825d9
freshman <cyrus@freshman.dev>
2024-12-26 05:47:57 -0500

    blueberry

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,43 @@
 import react from 'react'
 import styled from 'styled-components'
 import { asInput, new_trigger, S, useM, useS, useStored, useStyle } from './lib/use'
-const { named_log, colors, node } = window as any
+const { named_log, range, colors, node } = window as any
 const log = named_log('App')
 
 
-const trigger_color = new_trigger<string>(undefined)
-
 const App = () => {
-  const [count, set_count] = useStored('app-count', { default:0 })
-  const [count_2, set_count_2] = useStored('app-count', { default:0 })
-  const [color, set_color] = trigger_color.use()
-  const color_readable = useM(color, () => {
-    const test_node = node(`<div style="color:${color};"></div>`)
-    document.body.appendChild(test_node)
-    const rgb = getComputedStyle(test_node).color
-    test_node.remove()
-    return colors.readable(colors.to_hex(rgb))
-  })
-  const [text, set_text, fill_text] = asInput(useS('hello world'))
-
-  useStyle(color, `
-  #app {
-    ${color ? `
-    button, input {
-      background: ${color};
-      color: ${color_readable};
-    }
-    ` : ''}
+  let [hidden, set_hidden] = useStored('blueberry-hidden', { default:{} })
+  let count = useM(hidden, () => Object.values(hidden).filter(Boolean).length)
+  useStyle(`
+  :root {
+    filter: none;
+    background: #6a6aff;
+    color: #226;
+    font-weight: bold;
+    height: 100%;
+    overflow: hidden;
+  }
+  #app button {
+    background: inherit;
   }
   `)
   return <Style id='app'>
-    <div className='column gap'>
-      <span style={S(`
-      white-space: pre-wrap;
-      `)}>{text||' '}</span>
-      <div className='row gap'>
-        <button className='cute' onClick={e => set_count(count + 1)}>count: {count}</button>
-        <button className='cute' onClick={e => set_count_2(count_2 + 1)}>count: {count_2}</button>
+    <div className='column gap' style={S(`
+    word-break: break-all;
+    `)}>
+      {/* {'blueberry • '.repeat(1e4)} */}
+      <div className='middle-row wide'>
+        <i className='cute' onClick={e => {
+          // set_hidden({})
+        }}>you have eaten <span onClick={e => set_hidden({})}>{count}</span> blueberr{count===1?'y':'ies'}</i>
       </div>
-      <div className='row gap'>
-        <button className='cute' onClick={e => set_color('red')}>red</button>
-        <button className='cute' onClick={e => set_color('gold')}>gold</button>
-        <button className='cute' onClick={e => set_color('green')}>green</button>
-        <button className='cute' onClick={e => set_color('blue')}>blue</button>
+      <div className='middle-row gap wrap'>
+        {range(1e3).map(i => <button key={i} className='cute' style={S(`
+        visibility: ${hidden[i] ? 'hidden' : 'visible'};
+        `)} onClick={e => {
+          set_hidden({ ...hidden, [i]: 1 })
+        }}>blueberry</button>)}
       </div>
-      <input className='cute' {...fill_text} />
-      <button className='cute' onClick={e => {
-        set_count(0)
-        trigger_color.reset()
-        set_text('hello world')
-      }}>reset</button>
     </div>
   </Style>
 }
